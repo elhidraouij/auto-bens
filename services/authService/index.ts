75018b3379143c59216df91a715c4b82239c25b4
elhidraouij <jawad.elhidraoui.pro@gmail.com>
2024-05-25 10:25:58 +0200

    update

--- services/authService/index.ts
+++ services/authService/index.ts
@@ -6,6 +6,7 @@ interface UserJwtPayload {
     iat: number
 }
 
+// Vérifier l'authentification 
 export const verifyAuth = async (token: string) => {
     try {
         const verified = await jwtVerify(token, new TextEncoder().encode(secret_key))
